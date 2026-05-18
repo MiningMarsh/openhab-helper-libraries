4aef70c5d5b6015cf65783986d0a1c0bb07b6f60
MiningMarsh <miningmarsh@protonmail.com>
2026-05-18 19:40:27 -0400

    Add example

---
+++ automation/python/home.py
@@ -0,0 +1,679 @@
+from mmdev.manager import Manager
+from devices.humidifier import Humidifier
+from devices.thermostat import Thermostat
+from devices.light import Light
+from devices.purifier import Purifier
+from devices.regulator import Regulator
+from devices.humidityregulator import HumidityRegulator
+from devices.automaticswitch import AutomaticSwitch
+from devices.lock import Lock
+from mmdev.device import as_device
+import mmdev.rules as rules
+from mmdev.timer import Timer
+import datetime as dt
+import time 
+
+
+m = Manager()
+
+
+tvoc = m.state_for(
+    int, 'TVOC', default=0,
+    metadata={
+        'ga': ('Sensor', {
+            'name': 'Total VOC',
+            'roomHint': 'Bedroom',
+            'sensorName': 'VolatileOrganicCompounds',
+            'valueUnit': 'PARTS_PER_MILLION'
+        })
+    },
+    channel='mqtt:topic:sensor_station:tvoc'
+)
+
+
+bedroom_humidity = m.state_for(
+    float, 'BedroomHumidity', default=0.5,
+    channel='mqtt:topic:sensor_station:humidity'
+)
+
+
+bedroom_humidity_ga = m.state_for(
+    int, 'BedroomHumidityGA', default=50,
+    metadata={
+        'ga': ('HumiditySensor', {
+            'name': 'Bedroom Humidity',
+            'roomHint': 'Bedroom'
+        })
+    }
+)
+
+
+@rules.register
+@rules.pass_context
+@bedroom_humidity.on_change
+def bedroom_humidity_change(_, new):
+    bedroom_humidity_ga.command = new * 100.0
+
+
+closet_humidity = m.state_for(
+    float, 'ClosetHumidity', default=0.5,
+    channel='mqtt:topic:closet_sensors:humidity'
+)
+
+
+primary_home = m.state_for(
+    bool, 'PrimaryHome', default=True,
+    channel='mqtt:topic:tile:primary_home'
+)
+
+
+spouse_home = m.state_for(
+    bool, 'SpouseHome', default=True,
+    channel='mqtt:topic:tile:spouse_home'
+)
+
+
+def scene(name_on, name_off, default=None, channel_on=None, channel_off=None):
+    off = m.state_for(
+        bool, 'Scene' + name_off.replace(' ', ''), 
+        default=not default,
+        channel=channel_off,
+        metadata={
+            'ga': ('Scene', {
+                'name': name_off,
+                'roomHint': 'OpenHAB'
+            })
+        }
+    )
+
+    on = m.state_for(
+        bool, 'Scene' + name_on.replace(' ', ''), 
+        default=default,
+        channel=channel_on,
+        metadata={
+            'ga': ('Scene', {
+                'name': name_on, 
+                'roomHint': 'OpenHAB'
+            })
+        }
+    )
+
+    @rules.register
+    @on.on_enable
+    def scene_on():
+        off.update = False
+
+    @rules.register
+    @off.on_enable
+    def scene_off():
+        on.update = False
+
+    return on
+
+
+def control(name, default):
+    return m.state_for(
+        bool, 'Control' + name.replace(' ', ''), default=default,
+        metadata={
+            'ga': ('Scene', {
+                'name': name,
+                'roomHint': 'OpenHAB'
+            })
+        }
+    )
+
+
+thermosleeping = scene('Thermostat Sleeping', 'Thermostat Awake', False)
+sleeping = scene('Sleeping', 'Awake', False, channel_on='mqtt:topic:sleep_mode:sleeping')
+
+
+@rules.register
+@rules.loop
+@sleeping.on_change
+def sleep():
+    now = dt.datetime.now()
+    minute, hour = now.minute, now.hour
+    thermosleeping.command = sleeping.value or (
+        hour <= 5
+        or hour > 20
+        or (
+            hour == 20 and
+            minute >= 30
+        )
+    )
+sleep()
+    
+
+away = scene('Away', 'Home', False)
+dog_home = scene('Dog Home', 'Dog Away', True, channel_on='mqtt:topic:tile:dog_home')
+
+
+@rules.register
+@dog_home.on_change
+@primary_home.on_change
+@spouse_home.on_change
+def away_change():
+    away.command = not(
+        primary_home.value
+        or spouse_home.value
+    )
+
+
+automatic_light_management = control('Automatic Light Management', True)
+automatic_air_purification = control('Automatic Air Purification', True)
+automatic_humidity_regulation = control('Automatic Humidity Regulation', True)
+
+automatic_management = control('Automatic Management', True)
+
+
+@rules.register
+@automatic_light_management.on_disable
+@automatic_air_purification.on_disable
+@automatic_humidity_regulation.on_disable
+def disable_automatic_management():
+    automatic_management.command = False
+
+
+@rules.register
+@automatic_management.on_enable
+def enable_automatic_management():
+    automatic_light_management.command = True
+    automatic_air_purification.command = True
+    automatic_humidity_regulation.command = True
+
+
+automatic_color_temperature = m.state_for(
+    int, 'AutomaticColorTemperature', default=6500,
+    channel="mqtt:topic:color_temperature:kelvin"
+)
+
+
+automatic_brightness = m.state_for(
+    float, 'AutomaticBrightness', default=1.0,
+    channel="mqtt:topic:color_temperature:brightness"
+)
+
+
+@rules.register
+@sleeping.on_change
+def sleeping_change():
+    automatic_management.command = True
+sleeping_change()
+
+
+
+lights = {
+    ('Living Room', 'Living Room Lights', True, True),
+    ('Hallway', 'Hallway Light', True, True),
+    ('Bathroom', 'Bathroom Lights', True, True),
+    ('Bedroom', 'Bedroom Lights', True, False),
+    ('Closet', 'Closet Light', True, False)
+}
+
+
+def light(room, name, group, has_motion):
+    if has_motion:
+        #detector = m.device_for(
+        #    TimedLatch, room, 'Motion Detector', target=True,
+        #    energized_channel="hue:0107:primary:motionsensors_" + room.replace(' ', '').lower() + ":presence",
+        #    timeout_default=300
+        #)
+        detector = None
+    else:
+        detector = None
+    dev = m.device_for(
+        Light, room, name,
+        sleeping_proxy=sleeping,
+        automatic_brightness_proxy=automatic_brightness,
+        automatic_color_temperature_proxy=automatic_color_temperature,
+        automatic_mode_proxy=automatic_light_management,
+        away_proxy=away,
+        dog_mode_proxy=dog_home,
+        motion_detected_proxy=detector and detector.energized,
+        color_channel=(
+            "hue:" 
+            + ('group' if group else '0210') 
+            + ":primary:bulbs_" 
+            + (
+                room.replace(' ', '').lower() 
+                if group else 
+                room.lower().replace(' ', '') + '_' + name.replace(' ', '').lower()
+            ) 
+            + ":color"
+        )
+    )
+
+    if detector is not None:
+        @rules.register
+        @automatic_light_management.on_enable
+        def enable_motion_detector():
+            detector.powered.command = True
+
+    remote_channel = "hue:0820:primary:dimmers_%s:dimmer_switch_event" % room.lower().replace(' ', '')
+
+    @rules.register
+    @rules.pass_context
+    @rules.on_trigger(remote_channel)
+    def remote_event(channel, event):
+        event = int(float(event))
+        if 1002 == event:
+            dev.controls.command = True
+        elif 2002 == event:
+            dev.controls.command = max(0, min(1, dev.controls.value[2] + 0.2))
+        elif 2003 == event:
+            dev.controls.command = 1
+        elif 3002 == event:
+            dev.controls.command = max(0.05, min(1, dev.controls.value[2] - 0.2))
+        elif 3003 == event:
+            dev.controls.command = 0.05
+        elif 4002 == event:
+            dev.controls.command = False
+
+
+for room, name, group, has_motion in lights:
+    light(room, name, group, has_motion)
+
+
+def purifier(room_name, device_name):
+    purifier_group = m.group_for(
+        room_name.replace(' ', '') + 'AirPurifier' + device_name.replace(' ', ''),
+        metadata={
+            'ga': ('AirPurifier', {
+                'lang': 'en',
+                'name': device_name,
+                'roomHint': room_name,
+                'fanModeName': 'Control Mode',
+                'fanModeSettings': 'Automatic=Automatic,Manual=Manual'
+            })
+        }
+    )
+
+    p = m.device_for(
+        Purifier, room_name, device_name,
+        controls_groups={purifier_group},
+        controls_metadata={'ga': ('fanSpeed', {})},
+        dog_mode_proxy=dog_home,
+        fan_mode_groups={purifier_group},
+        fan_mode_metadata={'ga': ('fanMode', {})},
+        fan_speed_channel='mqtt:topic:' + room_name.replace(' ', '').lower() + '_air_purifier:fan_speed',
+        filter_life_channel='mqtt:topic:' + room_name.replace(' ', '').lower() + '_air_purifier:filter_life',
+        filter_life_groups={purifier_group},
+        filter_life_metadata={'ga': ('fanFilterLifeTime', {})},
+        pm25_channel='mqtt:topic:' + room_name.replace(' ', '').lower() + '_air_purifier:pm25',
+        pm25_groups={purifier_group},
+        pm25_metadata={'ga': ('fanPM25', {})},
+        sleeping_proxy=sleeping,
+        tvoc_proxy=tvoc,
+        automatic_mode_proxy=automatic_air_purification
+    )
+
+    return p
+
+
+thermostat_purifier = purifier('Living Room', 'Living Room Air Purifier')
+bedroom_purifier = purifier('Bedroom', 'Bedroom Air Purifier')
+
+
+dumbpurifier_group = m.group_for(
+    'BedroomPurifier',
+    metadata={
+        'ga': ('AirPurifier', {
+            'lang': 'en',
+            'name': 'Backup Air Purifier',
+            'roomHint': 'Bedroom',
+            'fanModeName': 'Control Mode',
+            'fanModeSettings': 'Automatic=Automatic,Manual=Manual',
+        })
+    }
+)
+
+
+dumbpurifier = m.device_for(
+    AutomaticSwitch, 'Bedroom', 'Backup Purifier',
+    controls_groups={dumbpurifier_group},
+    controls_metadata={'ga': ('fanPower', {})},
+    mode_groups={dumbpurifier_group},
+    mode_metadata={'ga': ('fanMode', {})},
+    energized_channel='tplinksmarthome:hs103:outlet_dumbpurifier:switch'
+)
+
+
+@rules.register
+@automatic_air_purification.on_enable
+def automatic_air_purification_enabled():
+    dumbpurifier.mode.command = 'Automatic'
+
+
+@rules.register
+@rules.pass_context
+@dumbpurifier.mode.on_change
+def dumbpurifier_mode_change(_, change):
+    if change.lower().strip() != 'automatic':
+        automatic_air_purification.command = False
+
+
+@rules.register
+@bedroom_purifier.automatic_mode.on_change
+@bedroom_purifier.automatic_fan_speed.on_change
+def update_dumbpurifier():
+    dumbpurifier.monitor.command = (
+        bedroom_purifier.automatic_mode.value
+        and bedroom_purifier.automatic_fan_speed.value > 0.33
+    )
+
+
+thermostat = m.device_for(
+    Thermostat, 'Living Room', 'Thermostat',
+    away_proxy=away,
+    dog_mode_proxy=dog_home,
+    fan_mode_channel='zwave:honeywell_th6320zw_00_000:controller:thermostat:thermostat_fanmode',
+    humidity_channel='zwave:honeywell_th6320zw_00_000:controller:thermostat:sensor_relhumidity',
+    humidity_normalize=True,
+    mode_channel='zwave:honeywell_th6320zw_00_000:controller:thermostat:thermostat_mode',
+    overcool_channel='mqtt:topic:overcool:overcool',
+    overheat_channel='mqtt:topic:overheat:overheat',
+    setpoint_high_channel='zwave:honeywell_th6320zw_00_000:controller:thermostat:thermostat_setpoint_cooling',
+    setpoint_low_channel='zwave:honeywell_th6320zw_00_000:controller:thermostat:thermostat_setpoint_heating',
+    sleeping_proxy=thermosleeping,
+    temperature_channel='zwave:honeywell_th6320zw_00_000:controller:thermostat:sensor_temperature'
+)
+
+
+def humidifier(room_name, device_name, humidity, channel):
+    group = m.group_for(
+        f'Humidifier{room_name.replace(" ", "")}{device_name.replace(" ", "")}',
+        metadata={
+            'ga': ('Fan', {
+                'lang': 'en',
+                'name': device_name,
+                'roomHint': room_name,
+                'fanModeName': 'Control Mode',
+                'fanModeSettings': 'Automatic=Automatic,Manual=Manual',
+            })
+        }
+    )
+
+
+    h = m.device_for(
+        AutomaticSwitch, room_name, device_name,
+        controls_groups={group},
+        controls_metadata={'ga': ('fanPower', {})},
+        mode_groups={group},
+        mode_metadata={'ga': ('fanMode', {})},
+        energized_channel=channel
+    )
+
+    @rules.register
+    @automatic_humidity_regulation.on_enable
+    def automatic_humidity_regulation_enabled():
+        h.mode.command = 'Automatic'
+
+    @rules.register
+    @rules.pass_context
+    @h.mode.on_change
+    def humidifier_mode_change(_, change):
+        if change.lower().strip() != 'automatic':
+            automatic_humidity_regulation.command = False
+
+    @rules.register
+    @rules.pass_context
+    @humidity.on_change
+    def update_humidifier(_, new):
+        h.monitor.command = new < 0.5
+
+    return h
+
+
+livingroom_humidifier = humidifier(
+    'Living Room', 'Living Room Humidifier',
+    humidity=thermostat.humidity,
+    channel='tplinksmarthome:hs300:powerstrip_livingroom:outlet5#switch'
+)
+
+
+bedroom_humidity = m.state_for(
+    float, 'BedroomHumidity', default=0.5,
+    channel='mqtt:topic:closet_sensors:humidity',
+    dimension='Humidity',
+    metadata={
+        'ga': ('HumiditySensor', {
+            'name': 'Bedroom Humidity',
+            'roomHint': 'Bedroom'
+        })
+    }
+)
+
+
+bedroom_humidifier = humidifier(
+    'Bedroom', 'Bedroom Humidifier',
+    humidity=bedroom_humidity,
+    channel='tplinksmarthome:hs300:powerstrip_ac:outlet5#switch'
+)
+
+
+dehumidifier_group = m.group_for(
+    'Dehumidifier',
+    metadata={
+        'ga': ('Fan', {
+            'lang': 'en',
+            'name': 'Dehumidifier',
+            'roomHint': 'Bedroom',
+            'fanModeName': 'Control Mode',
+            'fanModeSettings': 'Automatic=Automatic,Manual=Manual',
+        })
+    }
+)
+
+
+dehumidifier = m.device_for(
+    Regulator, 'Bedroom', 'Dehumidifier',
+    increasing=False,
+    regulator_type=float,
+    window_default=0.05,
+    cooldown_period_default=300,
+    duty_period_default=300,
+    powered_default=True,
+    setpoint_default=0.5,
+    monitor_proxy=thermostat.humidity,
+    energized_channel='tplinksmarthome:hs103:outlet_dehumidifier:switch',
+    powered_groups={dehumidifier_group},
+    powered_metadata={'ga': ('fanPower', {})},
+    mode_groups={dehumidifier_group},
+    mode_metadata={'ga': ('fanMode', {})},
+)
+
+
+bedroom_temp = m.state_for(
+    int, "BedroomTemperature", default=0,
+    channel='mqtt:topic:closet_sensors:temperature',
+    dimension='Temperature',
+    metadata={
+        'ga': ('TemperatureSensor', {
+            'name': 'Bedroom Temperature',
+            'roomHint': 'Bedroom',
+            'useFahrenheit': True
+        })
+    }
+)
+
+
+bathroom_temp = m.state_for(
+    int, "BathroomTemperature", default=0,
+    channel='hue:0302:primary:temperaturesensors_bathroom:temperature',
+    dimension='Temperature',
+    metadata={
+        'ga': ('TemperatureSensor', {
+            'name': 'Bathroom Temperature',
+            'roomHint': 'Bathroom',
+            'useFahrenheit': True
+        })
+    }
+)
+
+
+bathroom_rugheater = m.state_for(
+    bool, 'BathroomRugHeater', default=True,
+    channel='tplinksmarthome:hs103:outlet_rugheater:switch'
+)
+
+
+
+@rules.register
+@rules.pass_context
+@bathroom_temp.on_change
+def rugheater_update(_, temp):
+    bathroom_rugheater.command = temp <= 72.0
+
+
+m.state_for(
+    int, "HallwayTemperature", default=0,
+    channel='hue:0302:primary:temperaturesensors_hallway:temperature',
+    dimension='Temperature',
+    metadata={
+        'ga': ('TemperatureSensor', {
+            'name': 'Hallway Temperature',
+            'roomHint': 'Hallway',
+            'useFahrenheit': True
+        })
+    }
+)
+
+
+@as_device(name='PortableAC', ephemeral=True)
+def PortableAC(device):
+    energized = device.property(bool, 'Energized', default=False)
+    powered = device.property(bool, 'Powered', default=True)
+    mode = device.property(str, 'Mode', default='Automatic')
+    last_transition = device.property(int, 'LastTransition', default=0)
+
+    @rules.register
+    @powered.on_command
+    def powered_command():
+        mode.command = 'Manual'
+
+    @rules.register
+    @energized.on_change
+    def portableac_last_transition():
+        last_transition.command = time.time()
+
+    @rules.register
+    @rules.loop
+    @bedroom_temp.on_change
+    @dehumidifier.energized.on_change
+    @last_transition.on_change
+    @powered.on_change
+    @thermostat.operation_mode.on_change
+    @thermostat.operation_setpoint_high.on_change
+    @mode.on_change
+    def portableac_update():
+        desired = dehumidifier.energized.value or (
+            'cool' in thermostat.operation_mode.value.lower() and
+            bedroom_temp.value > thermostat.operation_setpoint_high.value
+        )
+        if time.time() - last_transition.value < 15 * 60:
+            if mode.value.lower() == 'automatic':
+                powered.update = energized.value
+        elif mode.value.lower() == 'automatic':
+            energized.command = desired
+            powered.update = desired
+        else:
+            energized.command = powered.value
+    portableac_update()
+
+    return {energized, powered, mode}
+
+
+portable_ac_group = m.group_for(
+    'PortableAC',
+    metadata={
+        'ga': ('Fan', {
+            'lang': 'en',
+            'name': 'Portable AC',
+            'roomHint': 'Bedroom',
+            'fanModeName': 'Control Mode',
+            'fanModeSettings': 'Automatic=Automatic,Manual=Manual',
+        })
+    }
+)
+
+
+portableac = m.device_for(
+    device_class=PortableAC,
+    room_name='Bedroom',
+    device_name='Portable AC',
+    energized_channel='tplinksmarthome:hs300:powerstrip_ac:outlet2#switch',
+    mode_groups={portable_ac_group},
+    mode_metadata={'ga': ('fanMode', {})},
+    powered_groups={portable_ac_group},
+    powered_metadata={'ga': ('fanPower', {})}
+)
+
+
+m.device_for(
+    Lock, 'Hallway', 'Lock', timeout=300.0,
+    energized_channel='mqtt:topic:front_door_lock:locked',
+    energized_metadata={
+        'ga': ('Lock', {
+            'name': 'Front Door',
+            'roomHint': 'Hallway'
+        })
+    },
+)
+
+
+fan_group = m.group_for(
+    'BedroomFan',
+    metadata={
+        'ga': ('Fan', {
+            'lang': 'en',
+            'name': 'Bedroom Fan',
+            'roomHint': 'Bedroom',
+            'fanModeName': 'Control Mode',
+            'fanModeSettings': 'Automatic=Automatic,Manual=Manual',
+        })
+    }
+)
+
+
+
+fan = m.device_for(
+    AutomaticSwitch, 'Bedroom', 'Bedroom Fan',
+    controls_groups={fan_group},
+    controls_metadata={'ga': ('fanPower', {})},
+    mode_groups={fan_group},
+    mode_metadata={'ga': ('fanMode', {})},
+    energized_channel='tplinksmarthome:hs300:powerstrip_bedside:outlet6#switch'
+)
+
+
+@rules.register
+@dehumidifier.energized.on_change
+@portableac.energized.on_change
+@bedroom_purifier.automatic_mode.on_change
+@bedroom_purifier.automatic_fan_speed.on_change
+def update_fan():
+    fan.monitor.command = (
+        portableac.energized.value 
+        or dehumidifier.energized.value
+        or (
+            bedroom_purifier.automatic_mode.value
+            and bedroom_purifier.automatic_fan_speed.value >= 0.5
+        )
+    )
+update_fan()
+
+
+@rules.register
+@bedroom_purifier.automatic_mode.on_change
+@bedroom_purifier.automatic_fan_speed.on_change
+@thermostat_purifier.automatic_mode.on_change
+@thermostat_purifier.automatic_fan_speed.on_change
+def update_thermostat_fan_mode():
+    if all(p.automatic_mode.value for p in (bedroom_purifier, thermostat_purifier)):
+        if any(p.automatic_fan_speed.value > 0.25 for p in (bedroom_purifier, thermostat_purifier)):
+            thermostat.fan_mode.command = 'Auto'
+        else:
+            thermostat.fan_mode.command = 'Circulate'
+    else:
+        thermostat.fan_mode.command = 'Auto'
+update_thermostat_fan_mode()       
